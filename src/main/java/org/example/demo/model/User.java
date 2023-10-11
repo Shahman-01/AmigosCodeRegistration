011a4e1ee0a91bd568cf155bc115a859e2fe2da2
Shahman Magomadov <magomadovshahman@gmail.com>
2023-10-11 20:53:49 +0300

    mod User

--- src/main/java/org/example/demo/model/User.java
+++ src/main/java/org/example/demo/model/User.java
@@ -1,14 +1,26 @@
 package org.example.demo.model;
 
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Collection;
 import java.util.Collections;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
 public class User implements UserDetails {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String username;
@@ -17,6 +29,23 @@ public class User implements UserDetails {
 	private UserRole userRole;
 	private Boolean locked;
 	private Boolean enabled;
+
+	public User(String name,
+	            String username,
+	            String email,
+	            String password,
+	            UserRole userRole,
+	            Boolean locked,
+	            Boolean enabled) {
+
+		this.name = name;
+		this.username = username;
+		this.email = email;
+		this.password = password;
+		this.userRole = userRole;
+		this.locked = locked;
+		this.enabled = enabled;
+	}
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
